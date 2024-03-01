9be66e7de5679f623e483103e5ff4f2efbf9000b
Matthieu Bollot <m.bollot@code-troopers.com>
2024-03-01 17:57:12 +0100

    wip

--- src/js/cms-preview-templates/components/jumbotron.js
+++ src/js/cms-preview-templates/components/jumbotron.js
@@ -4,10 +4,10 @@ export default class Jumbotron extends React.Component {
   render() {
     const { title, subtitle } = this.props;
     return <div>
-      <h1 className="fr-h1">
+      <h1>
         {title}
       </h1>
-      <h2 className="fr-h2">
+      <h2>
         {subtitle}
       </h2>
     </div>
