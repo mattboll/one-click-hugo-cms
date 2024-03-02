abbf4e98cc462b8010d59e588e2f05bd3209b620
Matthieu Bollot <m.bollot@code-troopers.com>
2024-03-02 13:23:35 +0100

    wip lightbox

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 // JS Goes here - ES6 supported
 
 import "./css/main.scss";
+import "../node_modules/lightbox2/dist/js/lightbox-plus-jquery";
 
-// $('pre code:not([data-lang])').each(function (i, block) {
-//     hljs.highlightBlock(block);
-// });
-
-// import hljs from "../node_modules/highlight.js/es";
 
 const hljs = require("../node_modules/highlight.js/lib/common");
 hljs.highlightAll();
